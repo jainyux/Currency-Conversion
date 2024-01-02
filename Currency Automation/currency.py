c06bbe6d51e645af503f08287c4aff6f2fe1195b
Jainy Mary Joy <125820666+jainyux@users.noreply.github.com>
2024-01-02 22:14:08 +0000

    Add files via upload

---
+++ Currency Automation/currency.py
@@ -0,0 +1,37 @@
+import requests
+API_KEY = 'fca_live_s7iWQNRKbpqjyX4EeXVvOReHoTp4oz5xii9w50kE'
+BASE_URL = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
+CURRENCIES = ["USD", "CAD", "EUR", "AUD", "CNY"]
+
+def convert_currency(base):
+    currencies = ",".join(CURRENCIES)
+    url = f"{BASE_URL}&base_currency={base}&currencies={currencies}"
+    try:
+        response = requests.get(url, verify=False)
+        data = response.json()
+       
+        return data["data"]
+
+
+    except:
+        print("Invalid currency")
+        return None
+ 
+while True:
+    base = input("Enter the base currency (q for quit): ").upper()
+    if base == "Q":
+        break
+    data = convert_currency(base)
+    if not data:
+        continue
+    del data[base]
+    for ticker, value in data.items():
+        print(f"{ticker}: {value}")
+
+
+
+
+
+
+ 
+     
